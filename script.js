6f86ab919352aa7ed877a27a5e0e9f567fd1df41
Emanuel syngles <122393755+Emanuelsyngles@users.noreply.github.com>
2024-03-20 14:57:48 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,19 @@ const html = document.querySelector('html')
 const focobtn = document.querySelector('.app__card-button--foco')
 const curtobtn = document.querySelector('.app__card-button--curto')
 const longobtn = document.querySelector('.app__card-button--longo')
+const fundo = document.querySelector('.app__image')
 
 focobtn.addEventListener('click', () => {
     html.setAttribute('data-contexto', 'foco')
+    fundo.setAttribute('src','../imagens/foco.png')
 })
 
 curtobtn.addEventListener('click', () => {
     html.setAttribute('data-contexto', 'descanso-curto')
+    fundo.setAttribute('src', '../imagens/descanso-curto.png')
+})
+
+longobtn.addEventListener('click', () => {
+    html.setAttribute('data-contexto', 'descanso-longo')
+    fundo.setAttribute('src', '../imagens/descanso-longo.png')
 })
